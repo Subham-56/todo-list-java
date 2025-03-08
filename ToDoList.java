b572e3ae72e3de8cbe3b844cdab715bf75f5ec26
Subham Rungta <143800199+Subham-56@users.noreply.github.com>
2025-03-08 13:56:40 +0530

    Update ToDoList.java

--- ToDoList.java
+++ ToDoList.java
@@ -19,7 +19,7 @@ public class ToDoList {
             System.out.print("Choose an option: ");
             
             int choice = scanner.nextInt();
-            scanner.nextLine(); // consume newline
+            scanner.nextLine(); 
             
             switch (choice) {
                 case 1:
